cffcb31b13d07fea624ffbd8a79fa5b284e63124
Fallrainy <xie5405@163.com>
2019-01-02 17:04:48 +0800

    添加WLUIKit.h

--- Example/WLUIKit/WLViewController.m
+++ Example/WLUIKit/WLViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "WLViewController.h"
+#import "WLUIButton.h"
 
 @interface WLViewController ()
 
@@ -17,13 +18,30 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+    WLUIButton *button = [WLUIButton buttonWithType:UIButtonTypeCustom];
+    button.adjustsImageWhenHighlighted = NO;
+    [button setFixedImageSize:CGSizeMake(100, 100)];
+    button.frame = CGRectMake(100, 100, 100, 180);
+    button.titleLabel.font = [UIFont systemFontOfSize:15];
+    [button setTitleColor:[UIColor darkTextColor] forState:UIControlStateNormal];
+    [button setTitle:@"Girl" forState:UIControlStateNormal];
+    [button setLayout:WLUIButtonLayoutTitleLeftImageRight];
+    [button setTitleImageSpacing:10];
+    [button setImage:[UIImage imageNamed:@"girl"] forState:UIControlStateNormal];
+    
+    NSLog(@"intrinsicContentSize:%@",NSStringFromCGSize([button intrinsicContentSize]));
+    button.frame = ({
+        CGRect frame = button.frame;
+        frame.size = button.intrinsicContentSize;
+        frame;
+    });
+    button.backgroundColor = [UIColor cyanColor];
+    [self.view addSubview:button];
+    
+    
+    
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
---
+++ WLUIKit/Classes/WLUIKit.h
@@ -0,0 +1,15 @@
+//
+//  WLUIKit.h
+//  WLUIKit
+//
+//  Created by Fallrainy on 2018/9/11.
+//
+
+#ifndef WLUIKit_h
+#define WLUIKit_h
+
+#import "WLScrollView.h"
+#import "WLTableView.h"
+#import "WLUIButton.h"
+
+#endif /* WLUIKit_h */
